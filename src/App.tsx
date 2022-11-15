5f16aef84f47bd3178a2216da665bdc1c6de3251
Ludvig Lindahl <ludde.lindahl@gmail.com>
2022-11-15 22:56:13 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+import PortfolioCard from './PortfolioCard';
+import portfolio_periodic from './assets/portfolio_periodic.webp';
+import portfolio_exaples from './assets/portfolio_examples.webp';
+import portfolio_smallApps from './assets/portfolio_smallApps.webp';
+import portfolio_ragnarok from './assets/portfolio_ragnarok.webp';
+import portfolio_spaceApp from './assets/portfolio_spaceApp.webp';
+import portfolio_mealPlanner from './assets/portfolio_mealPlanner.webp';
+
+function App() {
+  return (
+    <div className='bg-bg h-full'>
+      <div className='flex flex-col bg-transparent w-fit mb-8 lg:mb-16 m-auto'>
+        <h1 className='text-center text-light font-medium text-base'>My Recent Work</h1>
+        <h2 className='text-center text-primaryAlt font-medium text-3xl'>Projects</h2>
+      </div>
+      <div className='w-11/12 md:w-3/4 m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 print:grid-cols-3'>
+        <PortfolioCard
+          header='The Periodic System in a interactive system'
+          link='https://github.com/luigi989/periodic'
+          demoLink='https://periodic.luigiworks.tech'
+          path={portfolio_periodic}
+        />
+        <PortfolioCard
+          header='SmallApps'
+          link='https://github.com/luigi989/smallApps'
+          demoLink='https://apps.luigiworks.tech'
+          path={portfolio_smallApps}
+        />
+        <PortfolioCard
+          header='Ragnarok'
+          link='https://github.com/luigi989/Ragnarok'
+          demoLink='https://ragnarok.luigiworks.tech'
+          path={portfolio_ragnarok}
+        />
+        <PortfolioCard
+          header='Examples in HTML/CSS'
+          link='https://github.com/luigi989/examples'
+          path={portfolio_exaples}
+        />
+        <PortfolioCard
+          header='Meal-Planner'
+          link='https://github.com/HawkieOne/Meal-Planner'
+          path={portfolio_mealPlanner}
+        />
+        <PortfolioCard
+          header='Space-app'
+          link='https://github.com/HawkieOne/space-app'
+          path={portfolio_spaceApp}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        //Dark mode
+        "bg": '#282828',
+        "bgAlt": '#1d2021',
+        "primary": '#98971a',
+        "primaryAlt": '#98971a',
+        "light": '#ebdbb2',
+
+        // Light mode
+        "liBg": '#e5e5f7',
+        "liBgAlt": '#e4e5f1',
+        "liPrimary": '#d2d3db',
+        "liSec": "#484b6a",
+        "liLight": '#9394a5',
+      },
+      textColor: {
+        primary: '#98971a',
+      },
+      backgroundImage: {
+        'bgTexture': "url('../src/assets/bg-texture.webp')",
+      },
+    },
+  },
+  plugins: [],
+}
